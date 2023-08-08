c414f2ec2d5199f00c57385d211a085f12cc82dd
arif <9104011907000004>
2023-08-08 09:35:18 +0700

    feat<logs>: create logs middleware

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
 const PORT = process.env.PORT || 5000
+
+// log middleware
+app.use(logger)
+
+app.use(express.json())
 
 // static file paths
 app.use('/', express.static(path.join(__dirname, 'public')))
